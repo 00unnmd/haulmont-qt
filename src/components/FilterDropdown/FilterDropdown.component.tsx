2aca733c883cb3b7f8adf3326f027624b4cca225
Aydar Karimov <zerozerounnamed@yahoo.com>
2020-09-22 15:17:51 +0500

    [HLMNT-13] linked data from filtersVariables and dropdown template.

--- src/components/FilterDropdown/FilterDropdown.component.tsx
+++ src/components/FilterDropdown/FilterDropdown.component.tsx
@@ -2,14 +2,20 @@ import React from 'react';
 
 import './styles.scss';
 
-export const FilterDropdown: React.FC = () => {
+interface IProps {
+  title: string;
+  variables: string[];
+}
+
+export const FilterDropdown: React.FC<IProps> = props => {
+  const { title, variables } = props;
   return (
     <div className='filter'>
-      <p className='filter-title'>Launch Site</p>
+      <p className='filter-title'>{title}</p>
       <select className='filter-dropdown'>
-        <option>123</option>
-        <option>456</option>
-        <option>789</option>
+        {variables.map((item: string, index) => (
+          <option key={index}>{item}</option>
+        ))}
       </select>
     </div>
   );
--- src/pages/Main/Main.page.tsx
+++ src/pages/Main/Main.page.tsx
@@ -25,8 +25,8 @@ export const Main: React.FC = () => {
         <h1>Launches</h1>
       </div>
       <div className='main-page-filters'>
-        <FilterDropdown />
-        <FilterDropdown />
+        <FilterDropdown title='Launch Site' variables={filtersVariables.launch_site_variables} />
+        <FilterDropdown title='Rocket' variables={filtersVariables.rocket_variables} />
       </div>
       <div className='main-page-missions'>
         <MissionList data={launches} />
